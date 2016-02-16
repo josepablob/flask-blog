eaac1b97ca9a80523206b5527aac90c9b6271432
Jose Bonilla <josepablob@gmail.com>
2016-02-16 14:07:17 -0500

    blog.py

--- sql.py
+++ sql.py
@@ -10,7 +10,7 @@ with sqlite3.connect("blog.db") as connection:
     # create the table
     c.execute("""CREATE TABLE posts (title TEXT, post TEXT)""")
 
-    #insert dummy data into the table
+    # insert dummy data into the table
     c.execute('INSERT INTO posts VALUES ("Good", "I\'m good.")')
     c.execute('INSERT INTO posts VALUES ("Well", "I\'m well.")')
     c.execute('INSERT INTO posts VALUES ("Excellent", "I\'m excellent.")')
