bf8aad30008578044691886abd8299559477afb0
Jose Bonilla <josepablob@gmail.com>
2016-02-19 17:04:54 -0500

    login and logout to the blog app

--- blog.py
+++ blog.py
@@ -7,6 +7,10 @@ import sqlite3
 
 # configuration
 DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = 'hard_to_guess'
+
 
 app = Flask(__name__)
 
@@ -19,13 +23,27 @@ def connect_db():
     return
 sqlite3.connect(app.config['DATABASE'])
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or \
+           request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            session['logged in'] = True
+            return redirect(url_for('main'))
+    return render_template('login.html', error = error)
 
 @app.route('/main')
 def main():
     return render_template('main.html')
+	
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
