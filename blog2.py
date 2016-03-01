ef1f025aea59aeec9b8a5a34384e19d13443b71d
Jose Bonilla <josepablob@gmail.com>
2016-02-29 19:09:32 -0500

    main.html, login.html and template.html

--- blog2.py
+++ blog2.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'hard_to_guess'
 
 app = Flask(__name__)
 
-# pulls in app configuration by looking for UPPERCASE variables
+# pulls in configuration by looking for UPPERCASE variables
 app.config.from_object(__name__)
 
 # function used for connecting to the database
@@ -36,10 +36,10 @@ def login():
     error = None
     if request.method == 'POST':
         if request.form['username'] != app.config['USERNAME'] or \
-           request.form['password'] != app.config['PASSWORD']:
+        request.form['password'] != app.config['PASSWORD']:
             error = 'Invalid Credentials. Please try again.'
         else:
-            session['logged in'] = True
+            session['logged_in'] = True
             return redirect(url_for('main'))
     return render_template('login.html', error=error)
 
@@ -51,7 +51,7 @@ def main():
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
-    flash('You were logged out')
+    flash('You were logged out.')
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
